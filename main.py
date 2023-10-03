023b3e8745423360ca576c370c8cff648f00fd95
Enrico Michelon <enrico.michelon@fracarro.com>
2023-10-03 07:46:14 +0200

    Class and commands

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from market import VeganMarket
 
+HELPER_STR="""
+I comandi disponibili sono i seguenti:
+aggiungi: aggiungi un prodotto al magazzino
+elenca: elenca i prodotto in magazzino
+vendita: registra una vendita effettuata
+profitti: mostra i profitti totali
+aiuto: mostra i possibili comandi
+chiudi: esci dal programma
+"""
+
+def add_product(vegan_market):
+    product = input("Nome del prodotto: ")
+    try:
+        quantity = int(input("Quantità: "))
+    except ValueError as e:
+        print(e)
+        return "Impossibile inserire la quantità desiderata."
+    
+    feedback_str = f"AGGIUNTO: {quantity} X {product}"
+    if vegan_market.is_in_store(product):
+        vegan_market.add(product, quantity)
+        return feedback_str
+    
+    try:
+        buy_price = float(input("Prezzo di acquisto: "))
+        sell_price = float(input("Prezzo di vendita: "))
+    except Exception as e:
+        print(e)
+        return "Il prezzo inserito non è corretto"
+    
+    vegan_market.add(product, quantity, (buy_price, sell_price))
+    return feedback_str
 
 if __name__ == "__main__":
     vegan_market = VeganMarket("vegan_market.json")
@@ -15,3 +47,21 @@ if __name__ == "__main__":
     aiuto: mostra i possibili comandi
     chiudi: esci dal programma  
     '''
+    while 1:
+        command = input("Inserisci un comando: ")
+        if command == "aiuto":
+            print(HELPER_STR)
+        elif command == "aggiungi":
+            print(add_product(vegan_market))
+        elif command == "elenca":
+            pass
+        elif command == "vendita":
+            pass
+        elif command == "profitti":
+            pass
+        elif command == "chiudi":
+            print("Bye bye")
+            break
+        else:
+            print(f"Comando non valido\n{HELPER_STR}")
+            
--- market/VeganMarket.py
+++ market/VeganMarket.py
@@ -1,11 +1,39 @@
+import json
+from os import path
+
 class VeganMarket():
     def __init__(self, market):
         '''
         market: file json contenente tutti i prodotti presenti nel market
         '''
         self._market = market
-    def add(self, product):
-        pass
+        self._gross_profit = 0
+        self._net_profit = 0
+        
+        if not path.isfile(self._market):
+            market_dict = {"products": {}}
+            
+            with open(self._market, "w", encoding='utf-8') as mrkt:
+                json.dump(market_dict, mrkt, indent=6)
+        
+    def is_in_store(self, product):
+        try:
+            mrkt = open(self._market, "r", encoding='utf-8')
+        except FileNotFoundError as err:
+            print("Il contenuto del magazzino non è attualmente disponibile")
+            return False
+        market_dict = json.load(mrkt)
+        if product in market_dict.keys():
+            return True
+        return False
+        
+    def add(self, product, quantity=1, price=()):
+        if not price:
+            return
+        else:
+            #with open(self._market, "w+", encoding='utf-8') as mrkt:
+            #    mmrkt
+            return   
     def get(self, product):
         pass
     def sell(self, product):
